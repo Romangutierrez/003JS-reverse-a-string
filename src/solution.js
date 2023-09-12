712ccf347b5e11f8310879c5cc1f3dac094a0a73
Roman Gutierrez <124226629+Romangutierrez@users.noreply.github.com>
2023-09-12 19:51:00 +0800

    stud-<RomanP.Gutierrez>.js

--- src/solution.js
+++ src/solution.js
@@ -1,4 +1,12 @@
 // Function to reverse a string
-function reverseString(_) {
-  // Put your code here
+function reverseString(str) {
+   let newString = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
 }
+const string = prompt('Enter a string: ');
+
+const result = reverseString(string);
+console.log(result);
